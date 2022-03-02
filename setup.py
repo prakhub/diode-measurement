046d87e2cdb31b0fe96bdaa26c27708263cdaa63
Bernhard Arnold <bernhard.arnold@cern.ch>
2022-03-02 14:57:20 +0100

    added setup.cfg

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='diode-measurement',
-    version='0.7.dev5',
-    author="Bernhard Arnold",
-    author_email="bernhard.arnold@oeaw.ac.at",
-    url="https://github.com/hephy-dd/diode-measurement",
-    packages=find_packages(exclude=['tests']),
-    install_requires=[
-        'PyQt5==5.15.*',
-        'PyQtChart==5.15.*',
-        'PyVISA==1.11.*',
-        'PyVISA-py==0.5.*',
-        'pint==0.17.*',
-        'json-rpc==1.13.*'
-    ],
-    package_data={},
-    entry_points={
-        'console_scripts': [
-            'diode-measurement = diode_measurement.__main__:main'
-        ]
-    },
-    test_suite='tests',
-    license='GPLv3'
-)
+setup()
